cb8c981c7f50f49b32e78ac9cac0a64e9925b09d
Britta Felzmann <brittafelzmann97@gmail.com>
2021-06-01 17:54:24 +0200

    Index and view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = policy_scope(User)
+    # authorize @users
 
+    if params[:query].present?
+      @users = User.where("location ILIKE ?", "%#{params[:query]}%")
+    else
+      # which params do are there when user clicks adventurous option?
+      @users = User.all
+    end
   end
 end
