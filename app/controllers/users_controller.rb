22782f0b9d4f33f856ed6f0a2010cdbb7146f08c
Britta Felzmann <brittafelzmann97@gmail.com>
2021-06-01 17:43:48 +0200

    Index method in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def index
+    @users = policy_scope(User)
+    # authorize @boats
 
+    if params[:query].present?
+      @users = User.where("location ILIKE ?", "%#{params[:query]}%")
+    else
+      # adventurous option => which params will be present?
+      @users = User.all
+    end
   end
 end
