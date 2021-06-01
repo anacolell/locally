9f92e3acf561da75e7cfa91ee87ddecaebfe823a
ana colell <anacolell8@gmail.com>
2021-06-01 17:15:20 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/search', to: 'pages#search'
+  resources :users, only: [:show, :index]
+  # resources :recommendations, only: [:new, :create, :index, :destroy] do
+  #   resources :bookmarks, only: [:new, :create, :destroy, :index]
+  #   resources :reviews, only: [:new, :create, :show, :edit, :update]
+  # end
+  # resources :conversations do
+  #   resources :messages, only: [:new, :create, :index, :destroy]
+  #   resources :meetups, only: [:new, :create, :edit, :update, :index, :show]
+  #   resources :bookmarks, only: [:create, :new]
+  # end
+
 end
