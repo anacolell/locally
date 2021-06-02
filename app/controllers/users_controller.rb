9a6927d3b0d70f2177150d7568505f41b3df64ee
Britta Felzmann <brittafelzmann97@gmail.com>
2021-06-02 17:53:17 +0200

    Started with the matching calculation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,29 @@
 class UsersController < ApplicationController
-  @tourist_interests_count = current_user.user_interests.count
-  @locals = User.where.not(id: current_user.id)
+  before_action :set_current_user_count
+  before_action :set_locals
+  # before_action :tourist_interest_count
 
   def index
     @users = policy_scope(User)
-    authorize @users
+    # @rating = calculate_similarity_ranking
+    # authorize @users
 
     if params[:query].present?
-      @users = User.where("location ILIKE ?", "%#{params[:query]}%")
-      @users.sort_by(&:calculate_similarity_ranking)
+      # @users = User.where("location ILIKE ?", "%#{params[:query]}%")
+      # @users.sort_by(&:calculate_similarity_ranking)
+
     else
+      raise
       # which params are there when user clicks adventurous option?
-      @users = User.all
+      # @users = User.where("location ILIKE ?", "%#{params[:query]}%")
+      # @users = User.all
+      # @users.sort_by(&:calculate_similarity_ranking)
+
+      @locals.sort_by do |local|
+        count_matching_interests(local)
+        # calculate_similarity_ranking
+      end
+      # @users = User.all
     end
   end
 
@@ -20,20 +32,23 @@ class UsersController < ApplicationController
   # end
 
 # method for calculating the number of similar interests between local and tourist
-  def count_matching_interests
-    @current_user_interests = current_user.user_interests
+  def count_matching_interests(local)
     # @locals = User.where.not(id: current_user.id)
     # @tourist_interests_count = @current_user_interests.count
-    @locals.each do |local|
-      @counter = 0
-      @locals_interests = local.user_interests
-      @current_user_interests.each do |interest_tourist|
-        @locals_interests.each do |local_interest|
-          @counter += 1 if interest_tourist == local_interest
-        end
+    shared_interests = local.user_interests.select do |local_interest|
+      current_user.user_interests.select do |tourist_interest|
+        tourist_interest.interest.id == local_interest.interest.id
       end
-    @counter
     end
+    shared_interests.length
+    # counter = 0
+    # locals_interests = local.user_interests
+    # current_user.user_interests.each do |interest_tourist|
+    #   locals_interests.each do |local_interest|
+    #     counter += 1 if interest_tourist == local_interest
+    #   end
+    # end
+    # counter
   end
 
 # method for calculating the similarity rating
@@ -51,6 +66,18 @@ class UsersController < ApplicationController
   end
 
   private
+
+  # def tourist_interest_count
+  #   @tourist_interests_count = @current_user_interests.count
+  # end
+
+  def set_locals
+    @locals = User.where.not(id: current_user.id)
+  end
+
+  def set_current_user_count
+    @tourist_interests_count = current_user.user_interests.count
+  end
 
   def user_params
     params.require(:user).permit(:name, :address, :found_on, :species)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_132045) do
+ActiveRecord::Schema.define(version: 2021_06_02_123609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_132045) do
     t.bigint "interest_category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["interest_category_id"], name: "index_interests_on_interest_category_id"
   end
 
