67175d0105f171bd308a5d3ce40068b5c8d3f21f
ana colell <anacolell8@gmail.com>
2021-06-08 17:57:58 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create, :show, :edit, :update]
   end
   resources :bookmarks, only: [:destroy]
-  resources :conversations, only: :show do
   resources :conversations, only: [:show, :index, :delete] do
     resources :messages, only: :create
   end
