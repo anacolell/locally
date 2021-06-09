5a63c030ce52ec18bb24a93bdab4b58bca3d08d8
Britta Felzmann <brittafelzmann97@gmail.com>
2021-06-09 17:17:42 +0200

    added has_many notifications

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,7 @@
 class Conversation < ApplicationRecord
   # belongs_to :user
   has_many :messages, dependent: :destroy
-
+  has_many :notifications
   belongs_to :tourist, class_name: "User"
   belongs_to :local, class_name: "User"
 end
